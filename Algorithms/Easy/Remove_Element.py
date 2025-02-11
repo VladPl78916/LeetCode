8bc89b9a5b138bb4dcf95458148bb661c7100d4d
4314 Плюснин В.Д. <vpd658@mail.ru>
2025-02-11 12:16:15 +0300

    Добавлена задача Remove Element

--- Algorithms/Easy/Remove_Element.py
+++ Algorithms/Easy/Remove_Element.py
@@ -6,7 +6,7 @@ class Solution(object):
             if nums[i] != val:
                 nums[k] = nums[i]
                 k += 1
-        return k, nums
+        return k
     
 sol  = Solution()
 print(sol.removeElement([3,2,2,3], 3))
@@ -16,6 +16,7 @@ class Solution(object):
     def removeElement(self, nums, val):
         while val in nums:
             nums.remove(val)
+        return nums
 
 sol  = Solution()
 print(sol.removeElement([3,2,2,3], 3))
